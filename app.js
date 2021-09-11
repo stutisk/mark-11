ea431b1c71a0671b5eeab939a5a701abaf53dbec
stutisk <48529714+stutisk@users.noreply.github.com>
2021-09-12 03:34:49 +0530

    chnages: final touch

---
+++ app.js
@@ -0,0 +1,42 @@
+
+const birthDay=document.querySelector("#date-of-birth");
+const luckyNum=document.querySelector("#lucky-number");
+const btncheck=document.querySelector("#check-number");
+const output = document.querySelector("#output-container");
+
+
+function checknumberislucky(){
+    const dob = birthDay.value;
+    const sum = calculateSum(dob);
+    console.log(sum);
+    if(sum&&dob)
+    compareValues(sum,luckyNum.value)
+    else{
+        output.innerText="both fields are required to be filled "
+    }
+
+
+}
+
+function calculateSum(dob){
+    dob = dob.replaceAll("-","");
+    let sum = 0;
+    for(let index=0;index<dob.length;index++){
+        sum = sum+ Number(dob.charAt(index));
+
+
+    }
+    return sum;
+}
+
+function compareValues(sum,luckyNum){
+    if(sum%luckyNum==0){
+    output.innerText ="ahan! your birthday is lucky 🥳🎈🎈🎈";
+    }else{
+      output.innerText ="oops!we are sorry 😮";
+    }
+    }
+
+    
+btncheck.addEventListener("click",checknumberislucky)
+
